c28a9861746f6d5ffddd14a8ea2c2c436a8f2605
ankitha <ankithac9606@gmail.com>
2025-04-29 15:02:54 +0530

    Adding jenkinsfile

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 from Library import register
 
-def test_cases();
+def test_cases():
     assert register("Ankitha", "Ankitha@gmail.com", "1234asdf") == "Registration successfull"
     assert register("Ankitha", "Ankitha", "1234asdf") == "Password must be 8 characters long"
     assert register("Ankitha", "Ankitha@gmail.com", "1234af") == "Invalid email"
