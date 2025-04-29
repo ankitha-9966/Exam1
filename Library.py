82d57a5f485e806a6574d43e11c75e208ffa4f78
ankitha <ankithac9606@gmail.com>
2025-04-29 14:53:05 +0530

    Adding new files

--- Library.py
+++ Library.py
@@ -1,17 +1,14 @@
 def register(name, email, password):
     if email.endswith("@gmail.com"):
         if len(password) >= 8:
-            if password.isalnum():
-                return "Registration successfull"
-            else:
-                return "Password must be alphanumeric"
+            return "Registration successfull"
+            
         else:
             return "Password must be 8 characters long"
     else:
-        print("Invalid email")
+        return "Invalid email"
 
-register("Ankitha", "Ankitha@gmail.com", "1234asdf")
-register("Ankitha", "Ankitha", "1234asdf")
-register("Ankitha", "Ankitha@gmail.com", "12343456")
-register("Ankitha", "Ankitha@gmail.com", "1234af")
+print(register("Ankitha", "Ankitha@gmail.com", "1234asdf"))
+print(register("Ankitha", "Ankitha", "1234asdf"))
+print(register("Ankitha", "Ankitha@gmail.com", "1234af"))
 
